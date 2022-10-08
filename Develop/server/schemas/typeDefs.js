9b37aba96841bbd5660d0f408417b0dfc7db6be8
Chris Newbold <chris@newboldcreative.com>
2022-10-08 22:41:06 +1100

    first commit

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,46 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    savebooks: [Book]
+  }
+
+  type Book {
+    _id: ID!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  input SavedBookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: SavedBookInput): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
